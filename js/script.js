eaef082aecf58cf8215ce075934a93720790a737
PerpetuumG <perpetuumg@mail.ru>
2021-10-19 15:55:43 +0300

    добавление картинок через js

--- js/script.js
+++ js/script.js
@@ -11,13 +11,12 @@ li.forEach(el => {
 })*/
 
 
-
 let navbar = document.querySelector(".navbar").querySelectorAll("a")
 // console.log(navbar)
 
 navbar.forEach(element => {
-    element.addEventListener('click', function() {
-        navbar.forEach(nav=>nav.classList.remove("active"))
+    element.addEventListener('click', function () {
+        navbar.forEach(nav => nav.classList.remove("active"))
 
         this.classList.add("active")
     })
@@ -31,4 +30,18 @@ navbar.forEach(element => {
 //         x.className = 'navbar';
 //     }
 // }
+
+
+let imageSources = ['/img/cap_1.png', '/img/cap_2.png', '/img/cap_3.png', '/img/cap_4.png']
+imageSources.forEach(element => {
+    let img = document.createElement("img")
+    img.width = '100'
+    img.height = '100'
+    img.style.marginLeft = '7px'
+    img.src = element
+    document.getElementById("cartin").appendChild(img)
+})
+
+
+
 
